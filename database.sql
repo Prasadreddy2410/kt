e88f7e0c567cf921794cd03deb30a98dd3080f32
Medagam Venkata Rajeswara Prasad Reddy <92919265+Prasadreddy2410@users.noreply.github.com>
2023-02-02 17:40:26 +0530

    firstcommit

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
 use kt;
 create table users(Username varchar(200),Password varchar(200),Email varchar(200),Confirmpassword varchar(200));
 insert into users (Username,Password,Email,Confirmpassword)values('abhi','abhi','abhi@gmail.com','abhi');
+create table theater(theaterid varchar(200),class_type varchar(200),no_of_seats varchar(200));
